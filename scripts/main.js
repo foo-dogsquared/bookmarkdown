f82174028c3850d81bdfe3e1f8a935a4546b865d
foo-dogsquared <34962634+foo-dogsquared@users.noreply.github.com>
2018-02-28 22:21:19 +0800

    Files are at mostly complete initial version now, all it needs to have is the function to be completely written and working.

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,60 @@ var urlInput = document.getElementById("inputLink"),
     savedLinks = document.getElementById("savedLinks");
 
 urlInput.value = '';
+nameInput.value = '';
 urlInput.focus();
+
+
+// list of links that the user saved
+var links = [];
 
 // adding links from the "Add Links" button
 function addLinks(e) {
     let list_item = document.createElement("li"),
-        list_links = document.createElement("a");
+        list_links = document.createElement("a"),
+        list_closeBtn = document.createElement("p");
     
     window.localStorage.setItem("links", urlInput.value);
+
     list_links.setAttribute("href", urlInput.value);
-    list_links.textContent = urlInput.value;
+    (nameInput.value) ? list_links.textContent = nameInput.value : list_links.textContent = urlInput.value;
+
+    list_closeBtn.setAttribute("class", "closeBtn");
+    list_closeBtn.textContent = "X";
 
     list_item.appendChild(list_links);
+    list_item.appendChild(list_closeBtn);
     list.appendChild(list_item);
+
+    pushObject()
+    urlInput.value = '';
+    nameInput.value = '';
 }
 
+// function for each of the closeBtn, also an event delegation for the <ul>
+function close(e) {
+    if (e.target && e.target.matches(".closeBtn")) {
+        savedLinks.removeChild(e.target);
+    }
+} 
+
+// creating the links as objects through a constructor
+function LinkValues(url, name) {
+    this.url = url;
+    this.name = name;
+}
+
+// pushing the object to the array that the browser will parse through when 
+// the page reloads or loads on another time
+function pushObject() {
+    // the new object to be pushed
+    let linkObject = new LinkValues(urlInput.value, nameInput.value);
+        localStorage.setItem("object1", linkObject);
+
+    links.push(linkObject);
+}
+
+// making the button do its intended work
 button.addEventListener("click", addLinks);
+
+savedLinks.addEventListener("click", close);
